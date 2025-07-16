08e21abde2dcda0b3c5d1eae1ba0d6fb767a6b51
ted111712 <ted111712@gmail.com>
2025-07-16 17:08:28 +0900

    Add(문제5): first commit

---
+++ Week 1/Problem 05/sort_calculator.py
@@ -0,0 +1,35 @@
+def main():
+    while True:
+        try:
+            numbers_input: list[float] = list(
+                map(float, input("하나 이상의 숫자를 입력하세요: ").split())
+            )
+            break
+        except ValueError:
+            print("다시 입력하세요.")
+    result: list[float] = sort_numbers(numbers_input)
+    print("Sorted:", *(f"<{value}>" for value in result))
+
+
+def sort_numbers(numbers: list[float]) -> list[float]:
+    """
+    ## 선택정렬
+    1. numbers 리스트를 arr로 복사
+    2. arr에서 가장 작은 수를 찾아 0번째 위치로 이동
+    3. arr[i:]로 범위를 줄여 단계 2번 반복
+    """
+    arr = numbers.copy()
+    n = len(arr)
+
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+    return arr
+
+
+if __name__ == "__main__":
+    main()
